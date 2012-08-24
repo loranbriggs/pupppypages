9221c7005539d0d23d732f86a1b28d870193ca54
Loran Briggs <loranbriggs@gmail.com>
2012-08-24 15:18:23 -0700

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Pupppypages::Application.config.secret_token = '8f2cc04818fb4369cbd0f5d559bd8b19df7c68db7ec3f9b892db452a23630def4b39f3e6d61e63a6de070843aad73ddce73d6906358cbaea3108696a7e1e0b3c'
